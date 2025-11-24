16ce94c23cd222cc60606987e4a6afc46365021a
Shreya Sharma <codershreya33@gmail.com>
2025-11-24 17:53:38 +0530

    Add spi_if interface definition
    Added spi_if interface bundling all SPI Master–Slave signals for cleaner connectivity in the SystemVerilog testbench.

---
+++ rtl/interface/spi_if.sv
@@ -0,0 +1,21 @@
+interface spi_if;
+
+    logic clk;
+
+    logic rst;
+
+    logic newd;
+
+    logic [11:0] din;
+
+    logic [11:0] dout;
+
+    logic done;
+
+    logic sclk;
+
+    logic cs;
+
+    logic mosi;
+
+endinterface
